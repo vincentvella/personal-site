270c176c17561ca73cf50d5a909eee251573778f
Vincent Vella <vincevella0@gmail.com>
2019-07-03 00:48:35 -0400

    Add card for attribute listings

---
+++ src/components/attribute-listing.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import get from 'lodash.get';
+import Img from 'gatsby-image';
+import { Card, CardContent, Typography, CardMedia } from '@material-ui/core';
+
+const AttributeListing = ({ node }) => {
+  const { id, frontmatter } = node;
+  const image = get(frontmatter, 'coverImage.childImageSharp.fluid', null);
+  console.log(image);
+  console.log({ id, frontmatter });
+  return (
+    <Card raised key={id}>
+      <CardContent>
+        <Img fluid={image} />
+        <Typography variant="h5">{frontmatter.title}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AttributeListing;
