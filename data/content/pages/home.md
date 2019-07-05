a870d36738b721d2dbc1f9de44671a148d1bf598
Vincent Vella <vincevella0@gmail.com>
2019-07-05 17:10:59 -0400

    Add linking from cards

--- data/content/pages/home.md
+++ data/content/pages/home.md
@@ -2,6 +2,6 @@
 title: 'Vincent Vella'
 subtitle: 'Web & Mobile Full-Stack Engineer'
 path: '/'
-body: "Welcome to my personal site! I'm a growing web and mobile developer. I built this site as a way to showcase some of my experience in the industry so far as well as depict my strengths and skills outside of my day-to-day work."
+body: "I believe that at it's core programming is fundamentally a creative activity. In the same way that a writer uses english and grammar to create books - programmers use their various programming languages and tools. As a growing web and mobile developer I intend to use this site as a way to showcase my creations and experience as well as assisting with the creative process of others throughout the field. Feel free to visit any of the links below to find out more about my background in the industry!"
 type: 'index'
 ---
--- src/components/attribute-listing.tsx
+++ src/components/attribute-listing.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import get from 'lodash.get';
 import Img from 'gatsby-image';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent, Typography, Button, CardActions } from '@material-ui/core';
+import { Link } from 'gatsby';
 
 const AttributeListing = ({ node }) => {
   const {
     id,
-    frontmatter: { title, coverImage }
+    frontmatter: { title, coverImage, path }
   } = node;
   const image = get(coverImage, 'childImageSharp.fluid', null);
   return (
@@ -15,6 +16,11 @@ const AttributeListing = ({ node }) => {
         <Img fluid={image} />
         <Typography variant="body1">{title}</Typography>
       </CardContent>
+      <CardActions>
+        <Link to={path} style={{ textDecoration: 'none' }}>
+          <Button size="small">See More</Button>
+        </Link>
+      </CardActions>
     </Card>
   );
 };
