39df6235da0f4db3a34c039826ab33279e63cc5f
Vincent Vella <vincevella0@gmail.com>
2019-07-07 19:15:18 -0400

    Add resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import get from 'lodash.get';
 import { graphql } from 'gatsby';
+import { Document, Page } from 'react-pdf';
 import { DefaultLayout } from '../layouts';
+import { Paper } from '@material-ui/core';
 
 const Resume = props => {
+  console.log('PROPS', props);
+  const [numPages, setNumPages] = React.useState(null);
+  const [pageNumber, setPageNumber] = React.useState(1);
   const contentScreens = get(props, 'data.allMarkdownRemark.edges', []);
+  const onPageLoadSuccess = ({ numPages }) => setNumPages(numPages);
+  const path = props.data.file.publicURL;
   return (
     <DefaultLayout drawerProps={contentScreens}>
-      <div>Resume</div>
+      <Paper>
+        <Document file={path} onLoadSuccess={onPageLoadSuccess} onLoadError={console.error}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </Paper>
     </DefaultLayout>
   );
 };
@@ -16,6 +27,10 @@ export default Resume;
 
 export const pageQuery = graphql`
   query {
+    file(relativePath: { eq: "resume/Resume.pdf" }) {
+      id
+      publicURL
+    }
     markdownRemark(frontmatter: { path: { eq: "/resume" } }) {
       frontmatter {
         title
