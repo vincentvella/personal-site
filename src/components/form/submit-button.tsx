0423ebb717fe5b4df14b9a660042dc4eb0648172
Vincent Vella <vincevella0@gmail.com>
2019-08-13 22:52:08 -0400

    Throw a cooldown timeout hook on the submit button

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -1,20 +1,51 @@
-import { Button, CircularProgress } from '@material-ui/core';
+import { Button, CircularProgress, Typography, Grid } from '@material-ui/core';
 import { connect } from 'formik';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+function useCoolDown(): [boolean, (value: boolean) => void] {
+  const [cooledDown, setCooledDown] = useState<boolean>(true);
+  useEffect(() => {
+    let coolDownTimer;
+    if (!cooledDown) {
+      coolDownTimer = setTimeout(() => setCooledDown(true), 300000);
+    }
+    return () => clearTimeout(coolDownTimer);
+  }, [cooledDown]);
+  return [cooledDown, setCooledDown];
+}
 
 const SubmitButton: React.FC<any> = ({ formik, children, ...props }) => {
+  const [cooledDown, setCooledDown] = useCoolDown();
   const { isValid, isSubmitting, handleSubmit } = formik;
+  const handleClick = (...formikStuff) => {
+    setCooledDown(false);
+    handleSubmit(...formikStuff);
+  };
   return (
-    <Button
-      color="primary"
-      disabled={!isValid || isSubmitting}
-      onClick={handleSubmit}
-      type="submit"
-      variant="contained"
-      {...props}
-    >
-      {isSubmitting ? <CircularProgress size={22} /> : children}
-    </Button>
+    <>
+      <Button
+        color="primary"
+        disabled={!isValid || isSubmitting || !cooledDown}
+        onClick={handleClick}
+        type="submit"
+        variant="contained"
+        {...props}
+      >
+        {isSubmitting ? <CircularProgress size={22} /> : children}
+      </Button>
+      {isValid && !isSubmitting && !cooledDown && (
+        <>
+          <Grid item xs={12} />
+          <Grid item md={3} />
+          <Grid item md={6}>
+            <Typography variant="caption" color="error">
+              It seems that you've recently submitted a request, please wait 5
+              minutes before sending another request
+            </Typography>
+          </Grid>
+        </>
+      )}
+    </>
   );
 };
 
