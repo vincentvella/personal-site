bc5b584300d9090373008e726c9fcab94752fc87
Vincent Vella <vincevella0@gmail.com>
2019-08-13 23:40:01 -0400

    Add Snackbar to display success message on form

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -1,62 +1,89 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { InputField, SubmitButton } from '../form';
-import { Grid } from '@material-ui/core';
+import { Grid, Snackbar } from '@material-ui/core';
 import axios from 'axios';
+import SnackbarItem from '../snackbar-item';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
     .email()
     .required(),
-  message: Yup.string().required()
+  message: Yup.string()
+    .min(25)
+    .required()
 });
 
-const handleSubmit = async (
-  values,
-  { setSubmitting, resetForm, setStatus }
-) => {
-  setSubmitting(true);
-  try {
-    await axios.post(
-      'https://us-central1-personal-site-d9f76.cloudfunctions.net/sendMail',
-      values
-    );
-    resetForm();
-    setStatus({ success: 'You did it!' });
-    setSubmitting(false);
-  } catch (serverError) {
-    setStatus({ serverError });
-    setSubmitting(false);
-  }
-};
+const ContactForm = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const handleSubmit = async (
+    values,
+    { setSubmitting, resetForm, setStatus }
+  ) => {
+    setSubmitting(true);
+    try {
+      await axios.post(
+        'https://us-central1-personal-site-d9f76.cloudfunctions.net/sendMail',
+        values
+      );
+      resetForm();
+      setOpen(true);
+      setSubmitting(false);
+    } catch (serverError) {
+      setStatus({ serverError });
+      setSubmitting(false);
+    }
+  };
 
-const ContactForm = () => (
-  <Formik
-    initialValues={{ email: '', message: '' }}
-    onSubmit={handleSubmit}
-    validationSchema={validationSchema}
-    render={() => (
-      <Form>
-        <Grid container>
-          <Grid item md={3} />
-          <Grid item xs={12} md={6}>
-            <InputField fullWidth name="email" label="Email" />
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item md={3} />
-          <Grid item xs={12} md={6}>
-            <InputField name="message" label="Message" multiline />
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item md={3} />
-          <SubmitButton>Submit</SubmitButton>
-        </Grid>
-      </Form>
-    )}
-  />
-);
+  function handleClose(event?: SyntheticEvent, reason?: string) {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  }
+
+  return (
+    <>
+      <Formik
+        initialValues={{ email: '', message: '' }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+        render={() => (
+          <Form>
+            <Grid container>
+              <Grid item md={3} />
+              <Grid item xs={12} md={6}>
+                <InputField fullWidth name="email" label="Email" />
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item md={3} />
+              <Grid item xs={12} md={6}>
+                <InputField name="message" label="Message" multiline />
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item md={3} />
+              <SubmitButton>Submit</SubmitButton>
+            </Grid>
+          </Form>
+        )}
+      />
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <SnackbarItem
+          onClose={handleClose}
+          variant="success"
+          message="Success, your message has been sent!"
+        />
+      </Snackbar>
+    </>
+  );
+};
 
 export default ContactForm;
