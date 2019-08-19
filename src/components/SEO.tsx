f5212fbd3fa80b139b54151f8998961d07a67045
Vincent Vella <vincevella0@gmail.com>
2019-08-18 20:29:52 -0400

    Bump react-helmet versions

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -19,12 +19,12 @@ const SEO = () => (
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
+            <meta property="og:type" content="website" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:creator" content={twitterUsername} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
-            <meta property="og:type" content="website" />
             <meta name="twitter:site" content={twitterUsername} />
           </Helmet>
         </>
