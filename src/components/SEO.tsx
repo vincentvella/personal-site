76e0e94d7ff95b0ea207e0376a3e3d1f6adb620c
Vincent Vella <vincevella0@gmail.com>
2019-08-19 20:13:12 -0400

    Add site address to the url to fix twitter card

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -14,17 +14,17 @@ const SEO = () => (
         <>
           <Helmet title={title}>
             <meta name="description" content={description} />
-            <meta name="image" content={image} />
+            <meta name="image" content={`${url}/${image}`} />
             <meta property="og:url" content={url} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
+            <meta property="og:image" content={`${url}/${image}`} />
             <meta property="og:type" content="website" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:creator" content={twitterUsername} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
+            <meta name="twitter:image" content={`${url}/${image}`} />
             <meta name="twitter:site" content={twitterUsername} />
           </Helmet>
         </>
